af3502b9aef25c211ef6deaef92e1c20a3e82f91
Sepideh <sepideh.badran05@gmail.com>
2024-06-17 18:57:00 +0330

    exersice21

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter the num");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int f1 = 1;
+
+int f2 = 1;
+
+int f3 = 1;
+
+int counter = 2;
+ while(counter < n)
+{
+    f3 = f1 + f2;
+    counter++;
+    f1 = f2;
+    f2 = f3;
+}
+Console.WriteLine(f3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
